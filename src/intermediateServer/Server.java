565ee428f41f9c26021bf83add580ed4e3747f35
Makarkin <makarkinstanislav@mail.ru>
2018-06-09 11:36:28 +0400

    10

--- src/intermediateServer/Server.java
+++ src/intermediateServer/Server.java
@@ -1,6 +1,7 @@
 package intermediateServer;
 
 import generalClasses.LongMessage;
+import intermediateServer.utils.PoolRequestHandler;
 import intermediateServer.utils.ServerClientSession;
 import intermediateServer.utils.UsersList;
 
@@ -15,7 +16,9 @@ public class Server extends Thread {
     private int port;
 
     private static UsersList usersList = new UsersList();
-    private static BlockingQueue<String> requestList = new ArrayBlockingQueue<>(10);
+    private static ArrayBlockingQueue<String> requestList = new ArrayBlockingQueue<>(10);
+
+    private PoolRequestHandler requestHandler;
 
     public synchronized static BlockingQueue<String> getRequestList() {
         return requestList;
@@ -39,54 +42,16 @@ public class Server extends Thread {
                 Socket clientSocket = serverSocket.accept();
                 System.out.println("New client connected to server");
                 ServerClientSession clientSession = new ServerClientSession(clientSocket);
+                if (requestList == null) {
+                     requestHandler = new PoolRequestHandler(requestList);
+                }
 
                 if (requestList.size() > 0) {
-                    String request = requestList.poll();
-                    String[] requestBody = request.split(" ");
-                    if ("fire".equals(requestBody[0])) {
-                        fireMethod(requestBody);
-                    } else if ("request".equals(requestBody[0])) {
-                        requestMethod(requestBody);
-                    }
+                    requestHandler.start();
                 }
             }
-        } catch (IOException | ClassNotFoundException e) {
+        } catch (IOException e) {
             e.printStackTrace();
-        }
-    }
-
-    private void requestMethod(String[] requestBody) throws IOException, ClassNotFoundException {
-        String requestTo = requestBody[1];
-        String requestFrom = requestBody[2];
-        String request = String.format("Do you want to play with %s?", requestFrom);
-        usersList.getUsers().get(requestTo).getThisObjectOutputStream().writeObject(new LongMessage(request));
-        LongMessage response = (LongMessage) usersList.getUsers().get(requestTo).getThisObjectInputStream().readObject();
-        if ("y".equals(response.getReport())) {
-            usersList.getUsers().get(requestFrom).setBusy(true);
-            usersList.getUsers().get(requestTo).setBusy(true);
-            usersList.getUsers().get(requestFrom).getThisObjectOutputStream().writeObject(new LongMessage("y " + requestTo));
-        } else {
-            usersList.getUsers().get(requestFrom).getThisObjectOutputStream().writeObject(new LongMessage("n"));
-        }
-    }
-
-    private void fireMethod(String[] requestBody) throws IOException {
-        int i = Integer.valueOf(requestBody[1]);
-        int j = Integer.valueOf(requestBody[2]);
-        String fireToName = requestBody[3];
-        String fireFromName = requestBody[4];
-        String resultForShooter;
-        String resultForUnderFire;
-        if (usersList.getUsers().get(fireToName).getBoard().getIndexCell(i, j).isWithShip()) {
-            resultForShooter = String.format("yourResult %s %s true", i, j);
-            resultForUnderFire = String.format("enemyResult %s %s true", i, j);
-            usersList.getUsers().get(fireToName).getThisObjectOutputStream().writeObject(new LongMessage(resultForUnderFire));
-            usersList.getUsers().get(fireFromName).getThisObjectOutputStream().writeObject(new LongMessage(resultForShooter));
-        } else {
-            resultForShooter = String.format("yourResult %s %s false", i, j);
-            resultForUnderFire = String.format("enemyResult %s %s false", i, j);
-            usersList.getUsers().get(fireToName).getThisObjectOutputStream().writeObject(new LongMessage(resultForUnderFire));
-            usersList.getUsers().get(fireFromName).getThisObjectOutputStream().writeObject(new LongMessage(resultForShooter));
         }
     }
 }
---
+++ src/intermediateServer/utils/PoolRequestHandler.java
@@ -0,0 +1,74 @@
+package intermediateServer.utils;
+
+import generalClasses.LongMessage;
+import intermediateServer.Server;
+
+import java.io.IOException;
+import java.util.concurrent.ArrayBlockingQueue;
+
+public class PoolRequestHandler extends Thread {
+
+    private ArrayBlockingQueue<String> poolRequest;
+
+    public PoolRequestHandler(ArrayBlockingQueue<String> requestList) {
+        poolRequest = requestList;
+    }
+
+    @Override
+    public void run() {
+        while (true) {
+            String request = poolRequest.poll();
+            String[] requestBody = request.split(" ");
+            if ("fire".equals(requestBody[0])) {
+                try {
+                    fireMethod(requestBody);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            } else if ("request".equals(requestBody[0])) {
+                try {
+                    requestMethod(requestBody);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                } catch (ClassNotFoundException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    private void requestMethod(String[] requestBody) throws IOException, ClassNotFoundException {
+        String requestTo = requestBody[1];
+        String requestFrom = requestBody[2];
+        String request = String.format("Do you want to play with %s?", requestFrom);
+        Server.getUserList().getUsers().get(requestTo).getThisObjectOutputStream().writeObject(new LongMessage(request));
+        LongMessage response = (LongMessage) Server.getUserList().getUsers().get(requestTo).getThisObjectInputStream().readObject();
+        if ("y".equals(response.getReport())) {
+            Server.getUserList().getUsers().get(requestFrom).setBusy(true);
+            Server.getUserList().getUsers().get(requestTo).setBusy(true);
+            Server.getUserList().getUsers().get(requestFrom).getThisObjectOutputStream().writeObject(new LongMessage("y " + requestTo));
+        } else {
+            Server.getUserList().getUsers().get(requestFrom).getThisObjectOutputStream().writeObject(new LongMessage("n"));
+        }
+    }
+
+    private void fireMethod(String[] requestBody) throws IOException {
+        int i = Integer.valueOf(requestBody[1]);
+        int j = Integer.valueOf(requestBody[2]);
+        String fireToName = requestBody[3];
+        String fireFromName = requestBody[4];
+        String resultForShooter;
+        String resultForUnderFire;
+        if (Server.getUserList().getUsers().get(fireToName).getBoard().getIndexCell(i, j).isWithShip()) {
+            resultForShooter = String.format("yourResult %s %s true", i, j);
+            resultForUnderFire = String.format("enemyResult %s %s true", i, j);
+            Server.getUserList().getUsers().get(fireToName).getThisObjectOutputStream().writeObject(new LongMessage(resultForUnderFire));
+            Server.getUserList().getUsers().get(fireFromName).getThisObjectOutputStream().writeObject(new LongMessage(resultForShooter));
+        } else {
+            resultForShooter = String.format("yourResult %s %s false", i, j);
+            resultForUnderFire = String.format("enemyResult %s %s false", i, j);
+            Server.getUserList().getUsers().get(fireToName).getThisObjectOutputStream().writeObject(new LongMessage(resultForUnderFire));
+            Server.getUserList().getUsers().get(fireFromName).getThisObjectOutputStream().writeObject(new LongMessage(resultForShooter));
+        }
+    }
+}
