f3fb89ed2c91a4ad2a303f4629ec059c6b30f636
Makarkin <makarkinstanislav@mail.ru>
2018-06-10 18:45:20 +0400

    455

--- src/intermediateServer/Server.java
+++ src/intermediateServer/Server.java
@@ -39,7 +39,6 @@ public class Server extends Thread {
                 Socket clientSocket = serverSocket.accept();
                 System.out.println("New client connected to server");
                 ServerClientSession clientSession = new ServerClientSession(clientSocket);
-                /*latch.await();*/
                 System.out.println("requestListSize " + requestList.size());
                 if (requestList.size() > 0) {
                     String request = requestList.poll();
@@ -53,9 +52,7 @@ public class Server extends Thread {
             }
         } catch (IOException /*| InterruptedException */| ClassNotFoundException e) {
             e.printStackTrace();
-        } /*catch (InterruptedException e) {
-            e.printStackTrace();
-        }*/
+        }
     }
 
     private void requestMethod(String[] requestBody) throws IOException, ClassNotFoundException {
